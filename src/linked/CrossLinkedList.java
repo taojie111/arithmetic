8818ca682133e95eb2c0c28fbec255bc8593021a
jyyh2018/yh_taojie <Tj668890>
2020-12-28 23:59:40 +0800

    。。。

--- src/linked/CrossLinkedList.java
+++ src/linked/CrossLinkedList.java
@@ -12,6 +12,7 @@ public class CrossLinkedList {
 
     public static void main(String[] args) throws IOException {
         MyLinkedList myLinkedList = new MyLinkedList();
+        // 输入[4,1]、[5,6,1]、[8,4,5],构造两个相交链表[4,1,8,4,5]、[5,6,1,8,4,5],相交点为8
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         String line;
         while ((line = in.readLine()) != null) {
@@ -32,6 +33,12 @@ public class CrossLinkedList {
         }
     }
 
+    /*
+    * 解题思路：双指针法
+    * 1、设置两个指针分别指向两个相交链表的头结点
+    * 2、移动两个指针，当指针到达链表尾指针指向的下一个节点时（也就是null），把指针指向另一个链表的头结点
+    * 3、当循环完一轮后，第二轮两个指针肯定会相遇（指向同一个节点），此时，该节点就是两个指针的相交点
+    * */
     public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         if (headA == null || headB == null) {
             return null;
