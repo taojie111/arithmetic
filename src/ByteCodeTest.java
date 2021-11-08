43cf7feb287d5b57d3b693230b99078993d9dcb3
taojie <1527938655@qq.com>
2021-11-08 22:09:18 +0800

    1

---
+++ src/ByteCodeTest.java
@@ -0,0 +1,7 @@
+public class ByteCodeTest {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+
+}
--- src/Test.java
+++ src/Test.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class Test {
 
     public static void main(String[] args) {
-        minWindow("ADOBECODEBANC", "ABC");
+        System.out.println("test");
     }
 
     static class User {
