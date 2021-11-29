91ad8352bac6ef7ae16b2ae25a9b0cb7af0af142
taojie <1527938655@qq.com>
2021-11-29 09:38:16 +0800

    1

--- src/Test.java
+++ src/Test.java
@@ -5,8 +5,15 @@ import java.util.*;
 
 public class Test {
 
-    public static void main(String[] args) {
-        System.out.println("test");
+    public static void main(String[] args) throws Exception {
+        char c1 = '严';
+        char c2='\u4E25';
+        char c3 = 97;
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+        String str = new String(new char[]{c1});
+        System.out.println(Arrays.toString(str.getBytes("UTF-8")));
     }
 
     static class User {
@@ -39,50 +46,6 @@ public class Test {
             User u = (User) obj;
             return this.getName().equals(u.getName());
         }
-    }
-    // 全局变量，记录递归函数的递归层数
-    public static int count = 1;
-
-    // 在递归函数的开头，调用 printIndent(count++) 并打印关键变量；然后在所有 return 语句之前调用 printIndent(--count) 并打印返回值。
-    // 递归函数的开头：printIndent(count++); System.out.println("i = %d, j = %d\n", i, j);
-    // 递归函数的结尾：printIndent(--count); System.out.println("return %d\n", res);
-    public static void printRecursion(String data, boolean flag) {
-        if (!flag) {
-            count--;
-        }
-        String s = "层出参：";
-        if (flag) {
-            s = "层入参：";
-        }
-        System.out.println(getBlank(count) + "第" + count + s + data);
-        if (flag) {
-            count++;
-        }
-    }
-
-    public static String getBlank(int n) {
-        String s = "";
-        if (n == 1) {
-        } else if (n == 2) {
-            s = "   ";
-        } else if (n == 3) {
-            s = "       ";
-        } else if (n == 4) {
-            s = "           ";
-        } else if (n == 5) {
-            s = "               ";
-        } else if (n == 6) {
-            s = "                   ";
-        } else if (n == 7) {
-            s = "                       ";
-        } else if (n == 8) {
-            s = "                           ";
-        } else if (n == 9) {
-            s = "                               ";
-        } else if (n == 10) {
-            s = "                                   ";
-        }
-        return s;
     }
 
     public static ListNode reverse(ListNode head) {
