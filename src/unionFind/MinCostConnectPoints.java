e3f1528eac03408c86fcef0d1b863906da9f8340
taojie <1527938655@qq.com>
2022-01-19 17:55:14 +0800

    1

---
+++ src/unionFind/MinCostConnectPoints.java
@@ -0,0 +1,96 @@
+package unionFind;
+
+import java.util.*;
+
+public class MinCostConnectPoints {
+
+    public static void main(String[] args) {
+        int[][] param = new int[5][2];
+        param[0] = new int[]{0, 0};
+        param[1] = new int[]{2, 2};
+        param[2] = new int[]{3, 10};
+        param[3] = new int[]{5, 2};
+        param[4] = new int[]{7, 0};
+        MinCostConnectPoints minCostConnectPoints = new MinCostConnectPoints();
+        int result = minCostConnectPoints.minCostConnectPoints(param);
+        System.out.println(result);
+    }
+
+    public int minCostConnectPoints(int[][] points) {
+        int n = points.length;
+        DisjointSetUnion dsu = new DisjointSetUnion(n);
+        List<Edge> edges = new ArrayList<Edge>();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                edges.add(new Edge(dist(points, i, j), i, j));
+            }
+        }
+        Collections.sort(edges, new Comparator<Edge>() {
+            @Override
+            public int compare(Edge edge1, Edge edge2) {
+                return edge1.len - edge2.len;
+            }
+        });
+        int ret = 0, num = 1;
+        for (Edge edge : edges) {
+            int len = edge.len, x = edge.x, y = edge.y;
+            if (dsu.unionSet(x, y)) {
+                ret += len;
+                num++;
+                if (num == n) {
+                    break;
+                }
+            }
+        }
+        return ret;
+    }
+
+    public int dist(int[][] points, int x, int y) {
+        return Math.abs(points[x][0] - points[y][0]) + Math.abs(points[x][1] - points[y][1]);
+    }
+
+    class DisjointSetUnion {
+        int[] f;
+        int[] rank;
+        int n;
+
+        public DisjointSetUnion(int n) {
+            this.n = n;
+            this.rank = new int[n];
+            Arrays.fill(this.rank, 1);
+            this.f = new int[n];
+            for (int i = 0; i < n; i++) {
+                this.f[i] = i;
+            }
+        }
+
+        public int find(int x) {
+            return f[x] == x ? x : (f[x] = find(f[x]));
+        }
+
+        public boolean unionSet(int x, int y) {
+            int fx = find(x), fy = find(y);
+            if (fx == fy) {
+                return false;
+            }
+            if (rank[fx] < rank[fy]) {
+                int temp = fx;
+                fx = fy;
+                fy = temp;
+            }
+            rank[fx] += rank[fy];
+            f[fy] = fx;
+            return true;
+        }
+    }
+
+    class Edge {
+        int len, x, y;
+
+        public Edge(int len, int x, int y) {
+            this.len = len;
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
