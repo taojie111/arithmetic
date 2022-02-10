e3a672692f773cee461377ecb2273ecfe9542aff
taojie <1527938655@qq.com>
2022-02-10 17:22:26 +0800

    1

---
+++ src/primary/array/MaxProfit.java
@@ -0,0 +1,15 @@
+package primary.array;
+
+public class MaxProfit {
+
+    public static void main(String[] args) {
+        int[] param = new int[]{7,1,5,3,6,4};
+        int result = doTest(param);
+        System.out.println(result);
+    }
+
+    public static int doTest(int[] prices) {
+
+        return 0;
+    }
+}
