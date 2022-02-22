067ec16c2b2bea6527dd38b9f747902da69a51a3
taojie <1527938655@qq.com>
2022-02-22 11:53:35 +0800

    1

---
+++ src/primary/characterString/CountAndSay.java
@@ -0,0 +1,13 @@
+package primary.characterString;
+
+public class CountAndSay {
+
+    public static void main(String[] args) {
+        String result = doTest(3);
+        System.out.println(result);
+    }
+
+    public static String doTest(int n) {
+        return null;
+    }
+}
