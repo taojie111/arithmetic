5740e67f3975f98570d4980431f6ef15ac650c3c
taojie <1527938655@qq.com>
2022-05-05 09:50:28 +0800

    1

--- src/medium/other/MajorityElement.java
+++ src/medium/other/MajorityElement.java
@@ -2,12 +2,13 @@ package medium.other;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class MajorityElement {
 
     public static void main(String[] args) {
         int[] param = new int[]{3,2,3};
-        int result = doTest(param);
+        int result = majorityElement2(param);
         System.out.println(result);
     }
 
@@ -33,5 +34,32 @@ public class MajorityElement {
 
         }
         return 0;
+    }
+
+    public static int majorityElement2(int[] nums) {
+        Random rand = new Random();
+
+        int majorityCount = nums.length / 2;
+
+        while (true) {
+            int candidate = nums[randRange(rand, 0, nums.length)];
+            if (countOccurences(nums, candidate) > majorityCount) {
+                return candidate;
+            }
+        }
+    }
+
+    private static int randRange(Random rand, int min, int max) {
+        return rand.nextInt(max - min) + min;
+    }
+
+    private static int countOccurences(int[] nums, int num) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == num) {
+                count++;
+            }
+        }
+        return count;
     }
 }
