0d363098a4e5abc7b1994fe7724f02cf310400f9
taojie <1527938655@qq.com>
2022-09-14 00:59:43 +0800

    1

--- src/wrong/Subsets.java
+++ src/wrong/Subsets.java
@@ -9,10 +9,11 @@ import java.util.List;
 public class Subsets {
 
     public static void main(String[] args) {
-        int[] param = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+        int[] param = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,1,1,1,1,1,1};
         List<List<Integer>> result = subsets(param);
         System.out.println(result.size());
         System.out.println(result.size() == dp(param));
+        System.out.println(result.size() == doTest(param));
     }
 
 
